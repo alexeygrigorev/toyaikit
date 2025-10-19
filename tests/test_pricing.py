b0a3158c6318c2fef975b6096d651ff0de9fa40a
gupta-vivek <23401083+gupta-vivek@users.noreply.github.com>
2025-10-19 11:40:45 +0000

    Correct test case function name in test_pricing

--- tests/test_pricing.py
+++ tests/test_pricing.py
@@ -1,10 +1,10 @@
 from toyaikit.pricing import PricingConfig
 from genai_prices import calc_price, Usage
 from unittest.mock import patch
-from decimal import Decimal
 import pytest
 from io import StringIO
 import sys
+
 
 class TestPricingConfig:
 
@@ -17,7 +17,7 @@ class TestPricingConfig:
         pricing_config2 = PricingConfig()
 
         assert self.pricing_config is pricing_config2
- 
+
     def test_calculate_cost_basic(self):
         """Test working of calculate cost function.
         """
@@ -26,9 +26,10 @@ class TestPricingConfig:
         model = 'gpt-5'
 
         genai_result = calc_price(
-                Usage(input_tokens=input_tokens, output_tokens=output_tokens), model_ref=model)
-        
-        pricing_config_result = self.pricing_config.calculate_cost(model=model, input_tokens=input_tokens, output_tokens=output_tokens)
+            Usage(input_tokens=input_tokens, output_tokens=output_tokens), model_ref=model)
+
+        pricing_config_result = self.pricing_config.calculate_cost(
+            model=model, input_tokens=input_tokens, output_tokens=output_tokens)
 
         assert pricing_config_result.input_cost == genai_result.input_price
         assert pricing_config_result.output_cost == genai_result.output_price
@@ -42,8 +43,9 @@ class TestPricingConfig:
         model = 'IamBatman'
 
         with pytest.raises(LookupError):
-            self.pricing_config.calculate_cost(model=model, input_tokens=input_tokens, output_tokens=output_tokens)
-    
+            self.pricing_config.calculate_cost(
+                model=model, input_tokens=input_tokens, output_tokens=output_tokens)
+
     def test_calculate_cost_negative_token(self):
         """Test calculate cost with negative token value.
         """
@@ -52,33 +54,34 @@ class TestPricingConfig:
         model = ''
 
         with pytest.raises(ValueError):
-            self.pricing_config.calculate_cost(model=model, input_tokens=input_tokens, output_tokens=output_tokens)
+            self.pricing_config.calculate_cost(
+                model=model, input_tokens=input_tokens, output_tokens=output_tokens)
 
     def test_list_all_models_with_provider_filter(self):
         """Test listing models with provider filter"""
 
         captured_output = StringIO()
         sys.stdout = captured_output
-        
+
         try:
             self.pricing_config.list_all_models(provider_filter="openai")
             output = captured_output.getvalue()
-            
+
             assert "openai:gpt-4o-mini: gpt 4o mini\n" in output
             assert "anthropic:claude-opus-4-0: Claude Opus 4\n" not in output
         finally:
             sys.stdout = sys.__stdout__
 
-    def test_list_all_models_with_wrong_provider_filter(self):
-        """Test listing models with wrong provider filter"""
+    def test_list_all_models_with_no_provider_filter(self):
+        """Test listing models with no provider filter"""
 
         captured_output = StringIO()
         sys.stdout = captured_output
-        
+
         try:
             self.pricing_config.list_all_models(provider_filter="IamBatman")
             output = captured_output.getvalue()
-            
+
             assert "openai:gpt-4o-mini: gpt 4o mini\n" in output
             assert "anthropic:claude-opus-4-0: Claude Opus 4\n" in output
             assert "x-ai:grok-3: Grok 3\n" in output
@@ -88,26 +91,26 @@ class TestPricingConfig:
     def test_list_all_models_with_wrong_provider_filter(self):
         """Test listing models with wrong provider filter"""
 
-        result = self.pricing_config.list_all_models(provider_filter="IamBatman")
+        result = self.pricing_config.list_all_models(
+            provider_filter="IamBatman")
         assert result is None
-        
 
     @patch('toyaikit.pricing.wait_prices_updated_sync')
     def test_update_price_success(self, mock_wait_prices):
         """Test successful price update and if default time out is 10."""
-        
+
         mock_wait_prices.return_value = True
         result = self.pricing_config.update_price()
-        
+
         mock_wait_prices.assert_called_once_with(10)
         assert result is True
 
     @patch('toyaikit.pricing.wait_prices_updated_sync')
     def test_update_price_fail(self, mock_wait_prices):
         """Test fail price update and custom timeout is getting passed."""
-        
+
         mock_wait_prices.return_value = False
         result = self.pricing_config.update_price(15)
-        
+
         mock_wait_prices.assert_called_with(15)
         assert result is False
